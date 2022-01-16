5ff718175d360e960abe37bae2bbf529885a734f
meteoratlas <kellyaccnts@gmail.com>
2022-01-16 12:19:16 -0700

    3d project start

---
+++ webg/webgl.js
@@ -0,0 +1,113 @@
+// Ensure ThreeJS is in global scope for the 'examples/'
+global.THREE = require("three");
+
+// Include any additional ThreeJS examples below
+require("three/examples/js/controls/OrbitControls");
+
+const canvasSketch = require("canvas-sketch");
+const random = require("canvas-sketch-util/random");
+const palettes = require("nice-color-palettes");
+
+const settings = {
+  // Make the loop animated
+  animate: true,
+  // Get a WebGL canvas rather than 2D
+  context: "webgl",
+  attributes: { antialias: true },
+};
+
+const sketch = ({ context }) => {
+  // Create a renderer
+  const renderer = new THREE.WebGLRenderer({
+    canvas: context.canvas,
+  });
+
+  // WebGL background color
+  renderer.setClearColor("aliceblue", 1);
+
+  // Setup a camera
+  const camera = new THREE.OrthographicCamera();
+  // camera.position.set(2, 2, 4);
+  // camera.lookAt(new THREE.Vector3());
+
+  // Setup camera controller
+  // const controls = new THREE.OrbitControls(camera, context.canvas);
+
+  // Setup your scene
+  const scene = new THREE.Scene();
+
+  // Setup a geometry
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+
+  const palette = random.pick(palettes);
+
+  for (let i = 0; i < 30; i++) {
+    // Setup a mesh with geometry + material
+    const material = new THREE.MeshStandardMaterial({
+      color: random.pick(palette),
+    });
+    const mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(
+      random.range(-1, 1),
+      random.range(-1, 1),
+      random.range(-1, 1)
+    );
+    mesh.scale.set(
+      random.range(-1, 1),
+      random.range(-1, 1),
+      random.range(-1, 1)
+    );
+    scene.add(mesh);
+  }
+
+  scene.add(new THREE.AmbientLight(`hsl(0, 8%, 40%)`));
+
+  const light = new THREE.DirectionalLight("white", 1);
+  light.position.set(0, 0, 4);
+  scene.add(light);
+
+  // draw each frame
+  return {
+    // Handle resize events here
+    resize({ pixelRatio, viewportWidth, viewportHeight }) {
+      renderer.setPixelRatio(pixelRatio);
+      renderer.setSize(viewportWidth, viewportHeight, false);
+      // camera.aspect = viewportWidth / viewportHeight;
+
+      const aspect = viewportWidth / viewportHeight;
+
+      // Ortho zoom
+      const zoom = 2.0;
+
+      // Bounds
+      camera.left = -zoom * aspect;
+      camera.right = zoom * aspect;
+      camera.top = zoom;
+      camera.bottom = -zoom;
+
+      // Near/Far
+      camera.near = -100;
+      camera.far = 100;
+
+      // Set position & look at world center
+      camera.position.set(zoom, zoom, zoom);
+      camera.lookAt(new THREE.Vector3());
+
+      // Update the camera
+      camera.updateProjectionMatrix();
+    },
+    // Update & render your scene here
+    render({ time }) {
+      // controls.update();
+      // mesh.rotation.y = time * 0.1;
+      renderer.render(scene, camera);
+    },
+    // Dispose of events & renderer for cleaner hot-reloading
+    unload() {
+      // controls.dispose();
+      renderer.dispose();
+    },
+  };
+};
+
+canvasSketch(sketch, settings);
