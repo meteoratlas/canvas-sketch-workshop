6054cfce5f8cbaa0e803d47f756cd58eddf3563c
meteoratlas <kellyaccnts@gmail.com>
2022-01-15 21:27:54 -0700

    basic grid example

--- gen-art/sketch.js
+++ gen-art/sketch.js
@@ -1,13 +1,38 @@
-const canvasSketch = require('canvas-sketch');
+const canvasSketch = require("canvas-sketch");
 
 const settings = {
-  dimensions: [ 2048, 2048 ]
+  dimensions: [2048, 2048],
 };
 
 const sketch = () => {
+  const makeGrid = () => {
+    const points = [];
+    const count = 5;
+    for (let x = 0; x < count; x++) {
+      for (let y = 0; y < count; y++) {
+        const u = count <= 1 ? 0.5 : x / (count - 1);
+        const v = count <= 1 ? 0.5 : y / (count - 1);
+        points.push([u, v]);
+      }
+    }
+    return points;
+  };
+
+  const points = makeGrid();
+
   return ({ context, width, height }) => {
-    context.fillStyle = 'white';
+    context.fillStyle = "white";
     context.fillRect(0, 0, width, height);
+
+    points.forEach(([u, v]) => {
+      const x = u * width;
+      const y = v * height;
+
+      context.beginPath();
+      context.lineWidth = 40;
+      context.arc(x, y, 100, 0, Math.PI * 2, true);
+      context.stroke();
+    });
   };
 };
 
