c23349d97daa1293b6b9384e822e3c07fb9b1481
meteoratlas <kellyaccnts@gmail.com>
2022-01-16 15:06:14 -0700

    colour noise

--- shader/shader.js
+++ shader/shader.js
@@ -16,17 +16,27 @@ const frag = glsl(/* glsl */ `
   uniform float aspect;
   varying vec2 vUv;
 
+  #pragma glslify: noise = require('glsl-noise/simplex/3d');
+  #pragma glslify: hsl2rgb = require('glsl-hsl2rgb');
+
   void main () {
-    vec3 colorA = vec3(0.0, 0.0, 1.0);
-    vec3 colorB = vec3(0.0, 1.0, 0.0);
+    // vec3 colorA = vec3(0.0, 0.0, 1.0);
+    // vec3 colorB = vec3(0.0, 1.0, 0.0);
 
     vec2 centre = vUv - 0.5;
-    centre.x *= aspect;
+    // centre.x *= aspect;
     float dist = length(centre);
 
-    float alpha = 1.0 - smoothstep(0.498, 0.5, dist);
-    vec3 color = mix(colorA, colorB, vUv.x);
-    gl_FragColor = vec4(color, alpha);
+    float alpha = 1.0 - smoothstep(0.497, 0.5, dist);
+    // vec3 color = mix(colorA, colorB, vUv.x);
+    // gl_FragColor = vec4(color, alpha);
+
+
+    float n = noise(vec3(centre * 2.0, time / 8.0));
+
+    vec3 colour = hsl2rgb(0.7 + n * 0.2, 0.5, 0.5);
+
+    gl_FragColor = vec4(colour, alpha);
   }
 `);
 
